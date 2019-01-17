a75b29710be87c1323467ee35de497812b331640
Fathur Hidayat <fathur.hidayat@gmail.com>
2019-01-17 12:40:22 +0700

    Updates

---
+++ precache-manifest.39a3b0c13a68b99706ae2e6cec251827.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3dd43e811002648e126",
+    "url": "/aj-quran/static/js/runtime~main.f3dd43e8.js"
+  },
+  {
+    "revision": "2fa1ebc04b9c45136cf8",
+    "url": "/aj-quran/static/js/main.2fa1ebc0.chunk.js"
+  },
+  {
+    "revision": "d34e87a39ff567535764",
+    "url": "/aj-quran/static/js/1.d34e87a3.chunk.js"
+  },
+  {
+    "revision": "2fa1ebc04b9c45136cf8",
+    "url": "/aj-quran/static/css/main.27be0ab6.chunk.css"
+  },
+  {
+    "revision": "56a713990c2c635c936f5a5a1b827a0b",
+    "url": "/aj-quran/index.html"
+  }
+];
